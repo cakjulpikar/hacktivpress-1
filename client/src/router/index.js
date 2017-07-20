41dfa2b11cf82f5f2f7f2dcc504ce75502fd45b1
Zulfikar Annur Ahmad <zlfikar.aa@gmail.com>
2017-07-20 14:57:29 +0700

    setting up vue

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import ArticleDetail from '@/components/ArticleDetail'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Hello',
-      component: Hello
+      path: '/:id',
+      name: 'ArticleDetail',
+      component: ArticleDetail
     }
   ]
 })

e3dab2814e4ed491b33c601f9724138ddb73f8ab
Zulfikar Annur Ahmad <zlfikar.aa@gmail.com>
2017-07-20 14:29:31 +0700

    delete article done

--- server/controllers/articles.js
+++ server/controllers/articles.js
@@ -1,4 +1,5 @@
 const Article = require('../models/Article')
+const ObjectId = require('mongodb').ObjectId
 
 const postArticle = function (req,res) {
   Article.create({
@@ -27,7 +28,37 @@ const getArticles = function (req,res) {
   })
 }
 
+const getArticle = function (req,res) {
+  Article.findOne({
+    _id: ObjectId(req.params.id)
+  })
+  .populate('author', 'username')
+  .exec(function (err,result) {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      res.send(result)
+    }
+  })
+}
+
+const deleteArticle = function (req,res) {
+  Article.findOne({
+    author: req.decoded._id,
+    _id: ObjectId(req.params.id)
+  }, function (err,result) {
+    result.remove(function (err,deleted) {
+      if (err) {
+        res.status(500).send(err)
+      } else {
+        res.send("This article delete"  + deleted)
+      }
+    })
+  })
+}
 module.exports = {
   postArticle,
-  getArticles
+  getArticles,
+  getArticle,
+  deleteArticle
 };
--- server/routes/articles.js
+++ server/routes/articles.js
@@ -4,5 +4,7 @@ const controller = require('../controllers/articles')
 
 router.post('/', jwtVerify.userVerify, controller.postArticle)
 router.get('/', controller.getArticles)
+router.get('/:id', controller.getArticle)
+router.delete('/:id', jwtVerify.userVerify, controller.deleteArticle)
 
 module.exports = router;
